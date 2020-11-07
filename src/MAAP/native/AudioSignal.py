5ab2cd6a9d83e0200875096179bffcecd98ed13a
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2020-11-07 12:04:54 +0000

    AudioSignal class plots signal

--- src/MAAP/native/AudioSignal.py
+++ src/MAAP/native/AudioSignal.py
@@ -1,6 +1,8 @@
 import sounddevice as sd
 import librosa
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 class AudioSignal():
     """"""
@@ -13,6 +15,7 @@ class AudioSignal():
 
         self.y = y
         self.sr = sample_rate
+        self.t = [1/self.sr * i for i in np.arange(0,len(self.y),1)]
         self.duration = librosa.get_duration(self.y, self.sr)
 
 
@@ -21,4 +24,23 @@ class AudioSignal():
 
     def get_duration(self):
         return self.duration
+
+
+    def plot_signal(self):
+        fig, ax = plt.subplots(1, 1)
+        step_seconds = 1/self.sr
+        t = [1/self.sr * i for i in np.arange(0,len(self.y),1)]
+        ax.set_xlabel("Time (s)")
+        ax.plot(t, self.y)
+        ax.grid(True)
+        ax.set_ylim(self.y.min(), self.y.max())
+        fig.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    t = np.arange(0,100,1)
+    y = np.sin(t * np.pi/20)
+    audioSignal = AudioSignal(y, 2000)
+    audioSignal.plot_signal()
 
