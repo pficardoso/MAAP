43d11915278a57e284946176701226ac94f350a9
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2020-11-29 18:57:24 +0000

    US22: script audio_acquisition: write a report of recording session

--- scripts/data_acquisition.py
+++ scripts/data_acquisition.py
@@ -28,9 +28,12 @@ import time
 import os
 import re
 import json
+import datetime
+import git
 
 FATHER_DIR_NAME = '../data.acquisition/'
 SUBFOLDER_PREFIX = 'set'
+REPORT_FILE_NAME = 'report.txt'
 
 example_text="" \
              "Examples:\n" \
@@ -43,6 +46,17 @@ parser.add_argument("-t", "--time", required=True, type=int, help="Duration of a
 parser.add_argument("-s", "--stop-condition", required=True, help="Stop condition of capture")
 parser.add_argument("-sp", "--stop-parameters", type=json.loads, help="Stop condition parameters - json input")
 parser.add_argument("-b", "--buffer-size", required=False, type=int, help="Size of audio buffer, in seconds")
+
+
+def get_current_date_time():
+
+    # datetime object containing current date and time
+    now = datetime.datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    return dt_string
+
+def get_str_minutes_second(seconds: int):
+    return "{} ({} seconds)".format(datetime.timedelta(seconds=seconds), seconds)
 
 
 def prepare_folders(dir_name):
@@ -83,10 +97,12 @@ def prepare_folders(dir_name):
                 wav_file_name = os.path.basename(src)
                 target = os.path.join(dir_to_move, wav_file_name)
                 os.rename(src, target)
+            ## move report
+            report_file_path = os.path.join(path_to_use, REPORT_FILE_NAME)
+            target = os.path.join(dir_to_move, REPORT_FILE_NAME)
+            os.rename(report_file_path, target)
 
     return path_to_use
-
-
 
 def capture( dir_name, segments_duration, stop_condition, stop_condition_parameters, buffer_size_duration,):
 
@@ -100,24 +116,53 @@ def capture( dir_name, segments_duration, stop_condition, stop_condition_paramet
     # waits two seconds to allow initialization of thread until audioReceiver
     # starts capture
     time.sleep(1)
-    c=0
+    counter_audios_recorded = 0
     while (audioReceiver.is_capturing()) or (audioReceiver.buffer_has_samples()):
         if not audioReceiver.buffer_has_samples():
-            c=c+1
+            counter_audios_recorded=counter_audios_recorded+1
             audio_signal = audioReceiver.get_sample_from_buffer()
-            AudioWriter(dir, str(c)+".wav", audio_signal).write()
+            audioWriter = AudioWriter(dir, str(counter_audios_recorded)+".wav", audio_signal).write()
 
+    return audioReceiver, audioWriter, counter_audios_recorded
 
+def make_report(capture_name, dir_name, run_date, stop_condition, segment_duration, audioReceiver: AudioReceiver , audioWriter: AudioWriter, nr_audios_recorded):
+    report = dict()
+    report["name"]                       = capture_name
+    report["date"]                       = run_date
+    report["audio_sample_rate"]          = "{} Hz".format(audioReceiver.get_sample_rate())
+    report["stop_condition"]             = stop_condition
+    report["stop_parameters"]            = audioReceiver.get_stop_condition_parameters()
+
+    buffer_size_seconds = audioReceiver.get_length_buffer_seconds()
+    if buffer_size_seconds == 0:
+        buffer_size_str = "inf"
+    else:
+        buffer_size_str = get_str_minutes_second(buffer_size_seconds)
+
+    report["buffer_size"]                = buffer_size_str
+    report["audio_segment_duration"]     = get_str_minutes_second(segment_duration)
+    report["total_segments_acquired"]    = nr_audios_recorded
+    total_time_acquired_seconds = segment_duration*nr_audios_recorded
+    report["total_time_acquired"]        = get_str_minutes_second(total_time_acquired_seconds)
+    report["MAAP_commit_sha"]              = git.Repo("../.").head.commit
+
+    with open(os.path.join(dir_name, REPORT_FILE_NAME), 'w') as file_writer:
+        for key, value in report.items():
+            file_writer.write("{}:\t{}\n".format(key, str(value)))
 
 if __name__=="__main__":
 
     args = parser.parse_args()
 
+    run_date = get_current_date_time()
+
     final_dir = prepare_folders(args.dir)
 
     print("Audio is being recorded")
-    capture(final_dir, args.time,
-            args.stop_condition, args.stop_parameters, args.buffer_size)
+    audioReceiver, audioWriter, nr_audios_recorded = capture(final_dir, args.time,
+                                args.stop_condition, args.stop_parameters,
+                                args.buffer_size)
 
+    make_report(args.dir, final_dir, run_date, args.stop_condition, args.time, audioReceiver, audioWriter, nr_audios_recorded)
     print("End")
 
