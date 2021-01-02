20320b42d3346ad6f17d0fe45425e0f0353712f7
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-01-02 23:28:12 +0000

    US42 (part1): AudioFeatureExtractor - configure feature extraction using config file. 1 - Created function that parses values from config files and saves them as instance properties; 2 - Created function that resets the configuration of instance

--- src/MAAP/native/AudioFeatureExtractor.py
+++ src/MAAP/native/AudioFeatureExtractor.py
@@ -3,8 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import re
+import configparser
 from src.MAAP.native.AudioSignal import AudioSignal
 from src.MAAP.native.AudioReader import AudioReader
+
+
+python_list_format_pattern = "\[((\s)*([a-zA-z1-9])+(\s)*(\,)?(\s)*)+\]"
+
+DEFAULT_OUTPUT_FORMAT = 'dict_key_per_feature'
+AVAILABLE_OUTPUT_FORMAT = ['dict_key_per_feature', 'dict_key_per_feature_category']
+AVAILABLE_KEYS_MAIN_SECTION_CONFIG = ["features", "outputformat"] # module config parser is case insensitive
 
 features_dict = dict()
 
@@ -24,8 +32,10 @@ class AudioFeatureExtractor():
         """Constructor for AudioFeatureExtractor"""
         self.audioSignal = None
         self.audio_file_path = None
-    ##def __repr__(self):
-    ##    pass
+        self._config_features = None
+        self._config_output_format = None
+        self._config_features_kwarg_dict = None
+        self._configured_by_file = False
 
     def load_audio_file(self, file_path):
 
@@ -41,6 +51,65 @@ class AudioFeatureExtractor():
         self.y = self.audioSignal.get_data()
         self.sample_rate = self.audioSignal.get_sample_rate()
 
+    def config_by_file(self, file_path):
+
+        conf = configparser.ConfigParser()
+        if not os.path.isfile(file_path):
+            raise Exception("{} does not exist or is not a file".format(file_path))
+
+        conf.read(file_path)
+
+        # check the values in the section main
+        if not set(conf["Main"]).issubset(set(AVAILABLE_KEYS_MAIN_SECTION_CONFIG)):
+            raise Exception(" Features with name(s) '{}' do not exist"
+                            .format(set(conf["Main"]).difference(set(AVAILABLE_KEYS_MAIN_SECTION_CONFIG))))
+
+        # get the features in main
+        features_value = conf['Main']['Features']
+        if re.match(python_list_format_pattern, features_value):
+            ##it is a string with a list format
+            features_list = re.sub("[\[\]\s]","",features_value).split(",")
+            ## Check if all features_list are available
+            if not set(features_list).issubset(set(features_dict)):
+                raise Exception(" Features with name(s) '{}' do not exist"
+                                .format(set(features_list).difference(set(features_dict))))
+        elif features_value.strip() == "all":
+            ## get all the features name
+            features_list = list(features_dict.keys())
+        else:
+            raise Exception("Value of Features must has python list format or value 'all'")
+
+        ##sort the list of features
+        features_list = sorted(features_list)
+
+        # get the output_format in main
+        output_format = DEFAULT_OUTPUT_FORMAT
+        if "OutputFormat" in conf["Main"]:
+            output_format = conf["Main"]["OutputFormat"]
+            if output_format not in AVAILABLE_OUTPUT_FORMAT:
+                raise Exception("OutputFormat '{}' not available. Allowed values are '{}'".format(output_format, AVAILABLE_OUTPUT_FORMAT))
+
+        ##define the section_names for each feature
+        feature_section_names = [name + "_func_kwargs" for name in features_list]
+
+        features_kwarg_dict = {}
+        for i in range(0, len(features_list)):
+            section_name = feature_section_names[i]
+            if not section_name in conf:
+                features_kwarg_dict[features_list[i]] = None
+                continue
+            features_kwarg_dict[features_list[i]] = dict(conf[section_name])
+
+        self._config_features = features_list
+        self._config_output_format = output_format
+        self._config_features_kwarg_dict = features_kwarg_dict
+        self._configured_by_file = True
+
+    def reset_config(self):
+        self._config_features = None
+        self._config_output_format = None
+        self._config_features_kwarg_dict = None
+        self._configured_by_file = False
 
     def compute_all_features(self):
         return {feature_name: feature_function(self) for feature_name, feature_function in features_dict.items()}
@@ -90,9 +159,12 @@ class AudioFeatureExtractor():
 if __name__=="__main__":
 
     audio_file_path = "../../../audio.files/sir_duke_fast.wav"
+    config_file_path = "/workspace/tmp/test.ini"
     print(features_dict)
     extractor = AudioFeatureExtractor()
     extractor.load_audio_file(audio_file_path)
     extractor.compute_spectrogram(True)
     print("Computing features")
     print(extractor.compute_all_features())
+
+    extractor.config_by_file(config_file_path)
