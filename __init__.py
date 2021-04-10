0c69c836c967f336b0fc837a01e7d55e2abb9f36
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-04-10 23:23:03 +0100

    Update __init__.py to import class AudioFeature

--- __init__.py
+++ __init__.py
@@ -8,4 +8,5 @@ from src.MAAP.native.AudioReceiver import AudioReceiver
 from src.MAAP.native.AudioSignal import AudioSignal
 from src.MAAP.native.AudioWriter import AudioWriter
 from src.MAAP.native.AudioCutter import AudioCutter
+from src.MAAP.native.AudioFeature import AudioFeature
 from src.MAAP.native import UtilsMAAP
