4258633f60c0923f601e398cd132b9090a929b0b
Pedro Cardoso <pedro.cardoso.code@gmail.com>
2021-11-10 23:25:30 +0000

    Temp commit - save this to ask some questions to carlos

--- src/MAAP/__init__.py
+++ src/MAAP/__init__.py
@@ -1,4 +1,26 @@
+__all__ = [
+    "AudioCutter",
+    "AudioFeature",
+    "AudioFeatureExtractor",
+    "AudioReader",
+    "AudioReceiver",
+    "AudioSignal",
+    "AudioWriter",
+    "utils",
+]
+
 import sys
+
+# import MAAP.utils
+# from MAAP.utils import func1 func2
+from MAAP.native import utils
+from MAAP.native.AudioCutter import AudioCutter
+from MAAP.native.AudioFeature import AudioFeature
+from MAAP.native.AudioFeatureExtractor import AudioFeatureExtractor
+from MAAP.native.AudioReader import AudioReader
+from MAAP.native.AudioReceiver import AudioReceiver
+from MAAP.native.AudioSignal import AudioSignal
+from MAAP.native.AudioWriter import AudioWriter
 
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
--- src/MAAP/native/utils/__init__.py
+++ src/MAAP/native/utils/__init__.py
@@ -1,2 +1,4 @@
-from src.MAAP.native.utils.AudioFeature2Tensor import audio_feature_2_tensor
-from src.MAAP.native.utils.concat_audio_signal import concat_audio_signals
+__all__ = ["audio_feature_2_tensor", "concat_audio_signals"]
+
+from MAAP.native.utils.AudioFeature2Tensor import audio_feature_2_tensor
+from MAAP.native.utils.concat_audio_signal import concat_audio_signals
